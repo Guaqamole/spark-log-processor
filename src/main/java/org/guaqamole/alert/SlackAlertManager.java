2cafd326b5b5a3d0cdad871240b85580c736cff8
Avokey <avoholo@gmail.com>
2024-07-03 20:24:49 +0900

    feat: SlackAlertManager

--- src/main/java/org/guaqamole/alert/SlackAlertManager.java
+++ src/main/java/org/guaqamole/alert/SlackAlertManager.java
@@ -1,4 +1,26 @@
 package org.guaqamole.alert;
 
+import com.slack.api.Slack;
+import com.slack.api.webhook.Payload;
+
+import java.io.IOException;
+
 public class SlackAlertManager {
+    private String webhookUrl;
+
+    public SlackAlertManager(String webhookUrl) {
+        this.webhookUrl = webhookUrl;
+    }
+    public void sendSlackAlert(String message) {
+        Slack slack = Slack.getInstance();
+        Payload payload = Payload.builder()
+                .text(message)
+                .build();
+        try {
+            slack.send(webhookUrl, payload);
+        } catch (IOException e) {
+            System.err.println("Error sending Slack alert: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
